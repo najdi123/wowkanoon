c79d9218c8ac34d1adcaf5698460fcb07c38bc37
monire <aglida1370@gmail.com>
2020-12-11 23:30:51 +0330

    initial commit

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -2,8 +2,6 @@ import React from 'react';
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import Slider from "react-slick";
-
-
 import img1 from '../../images/504.png'
 import img2 from '../../images/1100.png'
 import img3 from '../../images/GRE.png'
@@ -17,27 +15,21 @@ import img10 from '../../images/8.png'
 import img11 from '../../images/9.png'
 import img12 from '../../images/10.png'
 import img13 from '../../images/11.png'
+import rightArrow from '../../images/right-arrow.svg'
+import leftArrow from '../../images/left-arrow.svg'
 import styles from './carousel.module.css';
 
 function SampleNextArrow(props) {
     const {className, style, onClick} = props;
     return (
-        <div
-            className={className}
-            style={{...style, display: "block", background: "red"}}
-            onClick={onClick}
-        />
+        <img onClick={onClick} className={className} src={rightArrow}/>
     );
 }
 
 function SamplePrevArrow(props) {
-    const {className, style, onClick} = props;
+    const {className, onClick} = props;
     return (
-        <div
-            className={className}
-            style={{...style, display: "block", background: "green"}}
-            onClick={onClick}
-        />
+        <img onClick={onClick} className={`${className}`} src={leftArrow}/>
     );
 }
 
