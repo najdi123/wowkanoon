323bf13c90c0e5bc97a1a307eab2cf2541c3a409
monire <aglida1370@gmail.com>
2020-12-11 18:43:25 +0330

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar'
 import Layout from "../components/Layout";
 import React from "react";
+import Carousel from "../components/Carousel";
 
 const Home = () => {
     return (
         <Layout home>
             <div className="container">Home</div>
+            <Carousel/>
         </Layout>
     );
 };
