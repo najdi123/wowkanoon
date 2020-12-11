d38ecdb811479b953341c13a2a1b1f89401951f3
Ali Najdi <ali.najdi.1989@gmail.com>
2020-12-11 12:05:50 +0330

    navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">Home</div>
+        </div>
+    );
+};
+
+export default Home;
