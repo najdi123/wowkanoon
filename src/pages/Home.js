99d284fa14b514d17c8121663248b1bcaa1146bb
monire <aglida1370@gmail.com>
2020-12-12 11:59:22 +0330

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,15 @@ import Layout from "../components/Layout";
 import React from "react";
 import Carousel from "../components/carousel/Carousel";
 import Header from "../components/header/Header";
+import styles from "../components/header/header.module.css";
 
 const Home = () => {
     return (
         <Layout home>
-           <div style={{backgroundColor:'#33a3dc'}}>
-               <div className="container ">
-                   <Header/>
-               </div>
-           </div>
+            <div>
+                <Header/>
+                <div className={styles.btnStart}>Start</div>
+            </div>
             <div className="container mt-4">
                 <Carousel/>
             </div>
