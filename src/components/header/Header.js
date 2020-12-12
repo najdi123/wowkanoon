de1226faeed0f1322d3fef14a2e8f7449b374adb
monire <aglida1370@gmail.com>
2020-12-12 13:25:08 +0330

    fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,16 +9,17 @@ const Header = () => {
     return (
         <div className={styles.container}>
             <div className='d-flex flex-column justify-content-center align-items-center'>
-                <img src={logo}/>
+                <img style={{width:300}} src={logo}/>
                 <h1 className={styles.topHeader}>WORLD OF WORDS</h1>
                 <div className='d-flex align-items-center'>
                     <h2 className={styles.secondText}>Compete with </h2>
                     <h1 className={styles.number}>81600</h1>
                 </div>
                 <h2 className={styles.bottomHeader}>English learners Of Worlds</h2>
-                <div className='d-flex'>
-                    <img src={kanoon} className={styles.btn}/>
-                    <img src={kanoon} className={styles.btn}/>
+                <div className={styles.btnWrapper} style={{width:300, overflow:'hidden'}}>
+                    <img src={kanoon} className={`${styles.btn}`} />
+                    <div className={styles.space}/>
+                    <img src={kanoon} className={styles.btn} />
                 </div>
             </div>
             <div>
