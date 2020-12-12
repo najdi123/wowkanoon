6a13f6e941799e7d73a461708162cd6f54d8444f
Ali Najdi <ali.najdi.1989@gmail.com>
2020-12-12 12:20:04 +0330

    merging

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,16 +2,18 @@ import Layout from "../../components/Layout";
 import React from "react";
 import Carousel from "../../components/carousel/Carousel";
 import Header from "../../components/header/Header";
-import styles from "../../components/header/header.module.css";
+import styles from "./home.module.css";
 
 const Home = () => {
     return (
         <Layout home>
-            <div>
+            <div className='position-relative'>
                 <Header/>
                 <div className={styles.btnStart}>Start</div>
             </div>
+
             <div className="container mt-4">
+                <h3 className='text-center'>Play with 7666 words and compete with 59346 learners in World of Words (WoW)</h3>
                 <Carousel/>
             </div>
 
