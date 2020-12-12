3206869e9cba34b89bf7a69ea02ee039fa7559e4
monire <aglida1370@gmail.com>
2020-12-12 11:24:51 +0330

    initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './header.module.css'
+import logo from "../../images/headerLogo.png";
+import kanoon from "../../images/kanoondl.png";
+import mob1 from "../../images/mockHome.png";
+import mob2 from "../../images/mockTest.png";
+
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+                <img src={logo}/>
+                <h1>WORLD OF WORLDS</h1>
+             <div className='d-flex align-items-center'>
+                 <h2 className={styles.secondText}>Compete with </h2>
+                 <h1 className={styles.number}>81600</h1>
+             </div>
+                <h2 className={styles.secondaryText}>English learners Of Worlds</h2>
+                <div className='d-flex'>
+                    <img src={kanoon} className={styles.btn}/>
+                    <img src={kanoon} className={styles.btn}/>
+                </div>
+            </div>
+            <div>
+                <img src={mob1}/>
+                <img src={mob2}/>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
-import Navbar from '../components/Navbar'
 import Layout from "../components/Layout";
 import React from "react";
 import Carousel from "../components/carousel/Carousel";
+import Header from "../components/header/Header";
 
 const Home = () => {
     return (
         <Layout home>
-            <div className="container">Home</div>
-            <Carousel/>
+           <div style={{backgroundColor:'#33a3dc'}}>
+               <div className="container ">
+                   <Header/>
+               </div>
+           </div>
+            <div className="container mt-4">
+                <Carousel/>
+            </div>
+
+
         </Layout>
     );
 };
