a5b5677725e591eb798fa0b49882f477e50b2b40
Ali Najdi <ali.najdi.1989@gmail.com>
2020-12-12 13:50:51 +0330

    added register

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './styles/customStyle.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <Router>
      <Switch>
          <Route exact path="/" component={Home} />
+         <Route exact path="/register" component={Register} />
      </Switch>
     </Router>
   );
