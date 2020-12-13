16974379f6772c5c8ddce902eae28fd447576937
Ali Najdi <ali.najdi.1989@gmail.com>
2020-12-13 11:07:30 +0330

    moved axios from devdependecies to dependecies

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import  styles from './table.module.css'
-import axios from 'axios'
+import  styles from './table.module.css';
+import axios from 'axios';
 
 const Table = () => {
     const [data, setData] = useState([]);
